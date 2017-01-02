3d011a31d0196f58a97b18edd20ab65c7383b450
Fabrizio Steiner <stffabi@users.noreply.github.com>
2017-01-02 21:43:38 +0100

    Public containers with tags accordingly to the caddy version.

--- build.sh
+++ build.sh
@@ -44,3 +44,53 @@ docker build --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
              --rm=false \
              -t $IMAGE_NAME \
              .
+
+if [ "$2" == "" ]; then
+  echo "Do not deploy."
+  exit 0
+fi
+
+echo "Pushing $IMAGE_NAME..."
+docker push $IMAGE_NAME
+
+if [ "$2" != "release" ]; then
+  echo "Do not publish as release version."
+  exit 0
+fi
+
+push_docker() {
+  echo "  -> push $1 $2..."
+  docker tag $1 $2 || exit 1
+  docker push $2 || exit 1
+}
+
+IMAGE_NAME_ONLY=${IMAGE_NAME%%:*}
+if [ -z "$IMAGE_NAME_ONLY" ]
+then
+  echo "Unable to determine docker image name."
+  exit 1
+fi
+
+SEMVER=${CADDY_VERSION##Caddy }
+VERSION=`echo $SEMVER | awk '{split($0,a,"."); print a[1]}'`
+BUILD=`echo $SEMVER | awk '{split($0,a,"."); print a[2]}'`
+PATCH=`echo $SEMVER | awk '{split($0,a,"."); print a[3]}'`
+
+if [ "${VERSION}" = "" ]; then
+  echo "Please provide a semantic version."
+  exit 1
+fi
+
+if [ "${BUILD}" = "" ]; then
+  BUILD='0'
+fi
+
+if [ "${PATCH}" = "" ]; then
+  PATCH='0'
+fi
+
+echo "Pushing $IMAGE_NAME_ONLY..."
+push_docker $IMAGE_NAME             $IMAGE_NAME_ONLY:latest
+push_docker $IMAGE_NAME_ONLY:latest $IMAGE_NAME_ONLY:$VERSION
+push_docker $IMAGE_NAME_ONLY:latest $IMAGE_NAME_ONLY:$VERSION.$BUILD
+push_docker $IMAGE_NAME_ONLY:latest $IMAGE_NAME_ONLY:$VERSION.$BUILD.$PATCH
